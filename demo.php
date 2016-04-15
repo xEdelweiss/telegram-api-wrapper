7efc314848a47599889eb1a1f5e25a0d0787db89
Michael Sverdlikovsky <michael.sverdlikovsky@ab-soft.net>
2016-04-15 13:24:07 +0300

    Add few dev dependencies

--- demo.php
+++ demo.php
@@ -1,3 +1,16 @@
 <?php
 
-// @todo write code ;)
+require './vendor/autoload.php';
+
+// something goes here
+
+//region Helpers
+/**
+ * @param array ...$arguments
+ */
+function dd(...$arguments)
+{
+    call_user_func_array('dump', $arguments);
+    die;
+}
+//endregion
